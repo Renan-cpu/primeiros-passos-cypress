83dfcc97f74bc0447470368772569bbfd4d2ee06
Barbosa <renan.a.barbosa@capgemini.com>
2024-08-03 20:41:07 -0300

    Adicionando Objetos Seletores

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,23 +1,31 @@
 /// <reference types="Cypress" />
 
 describe('Orange HRM Tests', () => {
+
+  const selectorlist = {
+    userNameField: "input[name='username']",
+    passwordField: 'input[name="password"]',
+    loginButton: 'button[type="submit"]',
+    sectionTitleTopBar: '.oxd-topbar-header-breadcrumb-module',
+    wrongCredentialAlert: "[role='alert']"
+  }
+
   it('Login - Success', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get('input[name="username"]').type('Admin')
-    cy.get('input[name="password"]').type('admin123')
-    cy.get('button[type="submit"]').click()
+    cy.get(selectorlist.userNameField).type('Admin')
+    cy.get(selectorlist.passwordField).type('admin123')
+    cy.get(selectorlist.loginButton).click()
     cy.location('pathname').should('equal','/web/index.php/dashboard/index')
-    cy.get('.oxd-topbar-header-breadcrumb-module').contains('Dashboard')
+    cy.get(selectorlist.sectionTitleTopBar).contains('Dashboard')
   })
 
   it('Login - Fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get('input[name="username"]').type('Admin')
-    cy.get('input[name="password"]').type('admin123')
-    cy.get('button[type="submit"]').click()
+    cy.get(selectorlist.userNameField).type('Admin')
+    cy.get(selectorlist.passwordField).type('admin12')
+    cy.get(selectorlist.loginButton).click()
 
-    cy.get('p[class="oxd-text oxd-text--p oxd-alert-content-text"]').should('be.visible')
-
+    cy.get(selectorlist.wrongCredentialAlert)
   })
 
 })
